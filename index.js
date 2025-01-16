81dafff2b7d5f9991a02ad196d566ff2f1a3dbcf
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2025-01-16 15:42:24 +0700

    redirect

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/api.js";
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/url.js";
 
 if (getCookie("login")){
-    getJSON("https://asia-southeast2-awangga.cloudfunctions.net/florka/data/user","login",getCookie("login"),responseFunction);
+    getJSON("https://asia-southeast2-qrcreate-447114.cloudfunctions.net/qrcreate/data/user","login",getCookie("login"),responseFunction);
 }else{
     redirect("/login");
 }
@@ -12,8 +12,8 @@ if (getCookie("login")){
 function responseFunction(result){
     console.log(result);
     if (result.status === 200){
-        redirect("/dashboard");
+        redirect("/qr");
     }else{
-        redirect("/daftar");
+        redirect("/login");
     }
 }

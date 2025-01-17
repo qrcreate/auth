f74dabe3955cc5d53126e77ce851a80accf357ed
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2025-01-17 10:50:54 +0700

    console gget qcookie

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/api.js";
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/url.js";
 
 if (getCookie("login")){
+    console.log(getCookie("login"));
     getJSON("https://asia-southeast2-qrcreate-447114.cloudfunctions.net/qrcreate/data/user","login",getCookie("login"),responseFunction);
 }else{
     // redirect("/login");
